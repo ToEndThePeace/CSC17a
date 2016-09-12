e952c42a17b91b696b42dd53ea438c235682bf3f
rcc <rcc>
2016-09-12 13:05:20 -0700

    Added program template Signed-off-by: rcc <rcc>

---
+++ Program_Template/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Brandon Ramirez
+ * Created on September 12, 2016, 1:01 PM
+ * Purpose: Template for all your programs
+ */
+
+//System Libraries
+#include <cstdlib>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+
+//Program Execution
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process->Map input data to output
+    
+    //Display/Output Solution
+    
+    //Exit
+    return 0;
+}
+
+//Function Declarations
