b55e45bd78530d33d641905b4215624cf2afc5a2
rcc <rcc>
2016-09-12 14:05:40 -0700

    sdfhj Signed-off-by: rcc <rcc>

--- Class/09-12-2016/C_Pointers/main.cpp
+++ Class/09-12-2016/C_Pointers/main.cpp
@@ -25,7 +25,12 @@ int main(int argc, char** argv) {
     cout << *vals << endl << vals << endl;
     
     int *valptr = vals;
-    cout << valptr[1] << endl << valptr[0];
+    cout << valptr[1] << endl << valptr[0] << endl;
+    cout << *(vals + 2) << endl;
+    cout << *(valptr + 2) << endl << *(valptr + 1) << endl;
+    
+    int x, *xpt(&x);
+    cout << x << endl << xpt << endl << *xpt << endl;
     return 0;
 }
 
