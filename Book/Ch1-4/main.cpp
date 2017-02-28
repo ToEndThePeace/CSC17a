fd55bebaebe931466c63af88285983dca62d301c
ToEndThePeace <bran.ramirez.don@gmail.com>
2017-02-27 18:04:30 -0800

    Added Chapters 1-4 Book work Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

---
+++ Book/Ch1-4/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * Name: Brandon Ramirez
+ * Date: February 27, 2017
+ * CSC17A - Object-Oriented Programming (C++)
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    /*
+     * Review Problems
+     * Chapters 1-4
+     */
+    
+    /*
+     * Problem 3.12 
+     */
+    char letter;
+    cout << "Please enter a character to convert to ASCII: ";
+    cin >> letter;
+    cout << endl << "The ASCII code of your character is: " << int(letter) <<
+            endl << endl;
+    
+    return 0;
+}
+
