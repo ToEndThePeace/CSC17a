50061f71559649bf3e5434811d3e9e69d9d354ab
ToEndThePeace <bran.ramirez.don@gmail.com>
2017-03-20 11:54:29 -0700

    Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

--- Homework/Assignment02/Ch9/main.cpp
+++ Homework/Assignment02/Ch9/main.cpp
@@ -16,20 +16,63 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
+int *mmm(int *, int);
+
+
+//Basic Array Functions (Helpers)
+int *fillAry(int);
+void output(int *, int);
+void swap(int, int);
+void sort(int *);
 
 //Executable Code
-
 int main(int argc, char** argv) {
     //Menu
+    srand(time(0));
     
-    cout << "Which problem should I open?" << endl <<
+    /*cout << "Which problem should I open?" << endl <<
             "1. Mean, Median, and Mode" << endl <<
             "2. "
             ""
-            ">> ";
+            ">> ";*/
 
+    int s1 = 20;
+    int *a1 = fillAry(s1);
+    output(a1, s1);
+    
+    delete[] a1;
     //Return
     return 0;
 }
 
 //Function Declarations
+int *mmm(int *a, int n) {
+    
+}
+
+int *fillAry(int n) {
+    int *a = new int(0);
+    for (int i = 0; i < n; i++) {
+        a[i] = rand() % 90 + 10;
+    }
+    return a;
+}
+void output(int *a, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void swap(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void sort(int *a, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            if (a[i] > a[j]) swap(a[i], a[j]);
+        }
+    }
+}
