74da1db7c51d1d2119b9ccd2d75de378af1c35d1
ToEndThePeace <bran.ramirez.don@gmail.com>
2017-03-20 11:33:38 -0700

    Signed-off-by: ToEndThePeace <bran.ramirez.don@gmail.com>

---
+++ Book/Ch9_Pointers/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:    main.cpp
+ * Author:  Brandon Ramirez
+ * Created: Mar 17, 2017 2:47:46 PM
+ * Class:   CSC17A
+ * Purpose: Quick REview of Pointers
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Executable Code
+
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+
+    //Return
+    return 0;
+}
+
+//Function Declarations
---
+++ Homework/Assignment02/Ch9/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:    main.cpp
+ * Author:  Brandon Ramirez
+ * Created: Mar 17, 2017 2:16:52 PM
+ * Class:   CSC17A
+ * Purpose: To review the creation, declaration, and destruction of pointers
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Executable Code
+
+int main(int argc, char** argv) {
+    //Menu
+    
+    cout << "Which problem should I open?" << endl <<
+            "1. Mean, Median, and Mode" << endl <<
+            "2. "
+            ""
+            ">> ";
+
+    //Return
+    return 0;
+}
+
+//Function Declarations
