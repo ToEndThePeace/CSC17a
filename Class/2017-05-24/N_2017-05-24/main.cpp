8708e2a447aa5c09fd750b20c03b626d58ae5bc0
rcc <rcc>
2017-05-24 14:07:54 -0700

    Signed-off-by: rcc <rcc>

---
+++ Class/2017-05-24/N_2017-05-24/main.cpp
@@ -0,0 +1,57 @@
+/*
+ * GENERAL NOTES
+ * 
+ * matthew.treadway@gmail.com - send some kind of resume?? Fuck??
+ * 
+ * DO COMPUTER SCIENCE BROSEPH
+ * JUST DO IT
+ * 
+ * bro you can just be a web designer
+ * just
+ * fucking
+ * do
+ * it
+ * 
+ * staging.webgear.tk - DEGREE PATH
+ * 
+ * .NET Design Pattern Framework - book
+ * 
+ * dofactory.com/NET/design-patterns
+ *      Singleton
+ *      Iterator
+ *      
+ * 
+ */
+
+/* 
+ * 
+ * Stacks are implemented using classes, preferably a templated class
+ * so that any types can be utilized
+ * 
+ * STL stack CONTAINER
+ * stack< char, vector<char> > cstack;
+ * stack< char, list<char> > cstack;
+ * stack< char > cstack; //Using a deque??
+ * 
+ * 
+ * QUEUES
+ * 
+ * STL deque
+ * deque<char> cQueue;
+ * 
+ * STL queue
+ * queue<char> cQueue;
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
